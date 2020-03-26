de086531e0d7448f96e0ed1dd3ec4952a41ee6ff
taiki.mikami <mik036ta@gmail.com>
2020-03-26 18:05:32 +0000

    kadai

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,13 @@ class User < ApplicationRecord
     has_many :followings, through: :relationships, source: :follow
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
-
+    has_many :favorites
+    has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+    has_many :fav_microposts, through: :favorites, source: :micropost
+    has_many :reverses_of_fav_micropost, through: :reverses_of_favorite, source: :user
+    
+    
+    
     def follow(other_user)
         unless self == other_user
             self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -27,6 +33,25 @@ class User < ApplicationRecord
     
     def feed_microposts
         Micropost.where(user_id: self.following_ids + [self.id])
-    end    
+    end 
+    
+    def like(micropost)
+        self.favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+    
+    def unlike(micropost)
+        favorite = self.favorites.find_by(micropost_id: micropost.id)
+        favorite.destroy if favorite
+    end
+    
+    def like?(micropost)
+        self.fav_microposts.include?(micropost)
+    end
+    
+    def feed_fav_microposts
+        Micropost.where(user_id: self.fav_micropost_ids)
+    end
+    
+    
 
 end
